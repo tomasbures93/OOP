9b719836da163bf356271c0a2318cd6f1fcaee0b
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2025-01-26 18:25:34 +0100

    Projektdateien hinzufügen.

---
+++ OOP/Module/Socke.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Module
+{
+    internal class Socke
+    {
+        public string Farbe;
+        public bool Trocken;
+        public bool Sauber;
+
+        public Socke(string farbe, bool trocken, bool sauber)
+        {
+            Farbe = farbe;
+            Trocken = trocken;
+            Sauber = sauber;
+        }
+
+        public void Trockne()
+        {
+            if(Trocken == true)
+            {
+                Trocken = false;        // nicht trocken
+            } else
+            {
+                Trocken = true;         // Trocken
+            }
+        }
+
+        public void Wasche() 
+        { 
+            if(Sauber == true)
+            {
+                Sauber = false;         // nicht sauber
+            } else
+            {
+                Sauber = true;          // sauber
+            }
+        }
+
+        public void Ausgabe()
+        {
+            string statusTrocken;
+            string statusSauber;
+            if (Trocken == true)
+            {
+                statusTrocken = "ist";
+            } else
+            {
+                statusTrocken = "ist nicht";
+            }
+            if(Sauber == true)
+            {
+                statusSauber = "ist";
+            } else
+            {
+                statusSauber = "ist nicht";
+            }
+            Console.WriteLine($"Die Socken mit der Farbe {Farbe} {statusTrocken} trocken und {statusSauber} sauber.");
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP.Module;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socke roteSocke = new Socke("Rot", true, false);
+            Socke grueneSocke = new Socke("Grün", false, false);
+            Socke freshSocke = new Socke("Weiß", true, true);
+
+            roteSocke.Ausgabe();
+            grueneSocke.Ausgabe();
+            freshSocke.Ausgabe();
+
+            Console.WriteLine("Grüne socke gewascht und getrocknet");
+
+            grueneSocke.Trockne();
+            grueneSocke.Wasche();
+            grueneSocke.Ausgabe();
+        }
+    }
+}
