77a10d40052c88c1d601d309c81679c1239ebd73
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2025-01-27 22:50:07 +0100

    hausaufgabe fertig

--- Klassen - Bruchrechnung/Module/Bruch.cs
+++ Klassen - Bruchrechnung/Module/Bruch.cs
@@ -19,7 +19,7 @@ namespace Klassen___Bruchrechnung.Module
 
         public string Ausgabe()
         {
-            return $"{_zahler} / {_nenner}";
+            return $"{_zahler}/{_nenner}";
         }
 
         public Bruch Kehrwert()
@@ -29,33 +29,63 @@ namespace Klassen___Bruchrechnung.Module
         }
 
         public Bruch Kurzen()
-        {   
-            Bruch kurz = new Bruch();
-            return kurz;
+        {
+            int ggt = GGT(_zahler, _nenner);
+            int nenner = _nenner / ggt;
+            int zahler = _zahler / ggt;
+            return new Bruch(zahler, nenner);
         }
 
-        public Bruch Addieren(Bruch bruch1, Bruch bruch2)
+        public static Bruch Addieren(Bruch bruch1, Bruch bruch2)
         {
-            Bruch addition = new Bruch();
-            return addition;
+            int zahler = bruch1._zahler * bruch2._nenner + bruch2._zahler * bruch1._nenner;
+            int nenner = bruch1._nenner * bruch2._nenner;
+            return new Bruch(zahler, nenner);
         }
 
-        public Bruch Substrahieren(Bruch bruch1, Bruch bruch2)
+        public Bruch Substrahieren(Bruch bruch2)
         {
-            Bruch sub = new Bruch();
-            return sub;
+            int zahler = this._zahler * bruch2._nenner - bruch2._zahler * this._nenner;
+            int nenner = this._nenner * bruch2._nenner;
+            return new Bruch(zahler, nenner);
         }
 
-        public Bruch Dividieren(Bruch bruch1, Bruch bruch2)
+        public static Bruch Dividieren(Bruch bruch1, Bruch bruch2)
         {
-            Bruch div = new Bruch();
-            return div;
+            if(bruch1._nenner != 0 || bruch2._nenner != 0)
+            {
+                int zahler = bruch1._zahler * bruch2._nenner;
+                int nenner = bruch1._nenner * bruch2._zahler;
+                return new Bruch(zahler, nenner);
+            }
+            else
+            {
+                Console.WriteLine("Mit 0 darfst du nicht dividieren");
+                return new Bruch();
+            }
+
         }
 
-        public Bruch Multiplizieren(Bruch bruch1, Bruch bruch2)
+        public Bruch Multiplizieren(Bruch bruch2)
         {
-            Bruch multi = new Bruch();
-            return multi;
+            int zahler = this._zahler * bruch2._zahler;
+            int nenner = this._nenner * bruch2._nenner;
+            return new Bruch(zahler, nenner);
+        }
+
+        static int GGT(int a, int b)
+        {
+            int ergebniss = a % b;
+            if (ergebniss == 0)
+            {
+                return b;
+            }
+            else
+            {
+                a = b;
+                b = ergebniss;
+                return GGT(a, b);
+            }
         }
     }
 }
--- Klassen - Bruchrechnung/Program.cs
+++ Klassen - Bruchrechnung/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Klassen___Bruchrechnung.Module;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,107 @@ namespace Klassen___Bruchrechnung
     {
         static void Main(string[] args)
         {
+            string input;
+            string[] split;
+            int zahler = 0, nenner = 0;
+            Bruch bruch = new Bruch();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\tKlassen Bruchrechnung\n");
+                Console.WriteLine("Bitte geben sie Bruch ein ( 1/2 )");
+                input = Console.ReadLine();
+                if (input.Contains('/'))
+                {
+                    split = input.Split('/');
+                    if (int.TryParse(split[0], out zahler) && int.TryParse(split[1], out nenner))
+                    {
+                        bruch = new Bruch(zahler, nenner);
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("Wrong input - Bitte ziffern benutzen!\n");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fehler - Wrong input ... '/' missing!");
+                    Console.ReadKey();
+                }
+            } while (true);
+            Console.WriteLine($"\nBruch ausgabe: {bruch.Ausgabe()}");
+            Console.WriteLine($"Kehrwert Bruch: {bruch.Kehrwert().Ausgabe()}");
+            Console.WriteLine($"Bruch Kurzen: {bruch.Kurzen().Ausgabe()}\n");
+
+            Bruch bruch1 = new Bruch();
+            Bruch bruch2 = new Bruch();
+            Console.WriteLine("\n\tRechnen mit Bruch\n");
+            do
+            {
+                Console.WriteLine("Bitte geben sie Bruch nummer 1 ein in format ( 1/4 ):");
+                input = Console.ReadLine();
+                if (input.Contains('/'))
+                {
+                    split = input.Split('/');
+                    if (int.TryParse(split[0], out zahler) && int.TryParse(split[1], out nenner))
+                    {
+                        // wenn alles okay ist
+                        bruch1 = new Bruch(zahler, nenner);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input - Bitte ziffern benutzen!\n");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fehler - Wrong input ... '/' missing!\n");
+                    Console.ReadKey();
+                }
+            } while (true);
+            do
+            {
+                Console.WriteLine("Bitte geben sie Bruch nummer 2 ein in format ( 1/4 ):");
+                input = Console.ReadLine();
+                if (input.Contains('/'))
+                {
+                    split = input.Split('/');
+                    if (int.TryParse(split[0], out zahler) && int.TryParse(split[1], out nenner))
+                    {
+                        // wenn alles okay ist
+                        bruch2 = new Bruch(zahler, nenner);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input - Bitte ziffern benutzen!\n");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fehler - Wrong input ... '/' missing!\n");
+                    Console.ReadKey();
+                }
+            } while (true);
+
+            Console.WriteLine($"\n Ihre Bruche : {bruch1.Ausgabe()} und {bruch2.Ausgabe()}");
+            Bruch adition = Bruch.Addieren(bruch1, bruch2);
+            Console.WriteLine($"{bruch1.Ausgabe()} + {bruch2.Ausgabe()} = {adition.Kurzen().Ausgabe()}");
+
+            Bruch substra = bruch1.Substrahieren(bruch2);
+            Console.WriteLine($"{bruch1.Ausgabe()} - {bruch2.Ausgabe()} = {substra.Kurzen().Ausgabe()}");
+
+            Bruch multi = bruch1.Multiplizieren(bruch2);
+            Console.WriteLine($"{bruch1.Ausgabe()} * {bruch2.Ausgabe()} = {multi.Kurzen().Ausgabe()}");
+
+            Bruch division = Bruch.Dividieren(bruch1, bruch2);
+            Console.WriteLine($"{bruch1.Ausgabe()} / {bruch2.Ausgabe()} = {division.Kurzen().Ausgabe()}");
+
+            Console.ReadKey();
         }
     }
 }
